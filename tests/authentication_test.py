3e25542ed2b1004f87b1719f71fa8413c5ce3fa2
Samuel Kerr <kuzmatech@gmail.com>
2018-11-18 12:47:45 +0000

    wrote authentication tests - to be implemented

---
+++ tests/authentication_test.py
@@ -0,0 +1,13 @@
+import pytest
+import authentication
+
+def test_password_hashing():
+    examplepass = 'ilikegreeneggsandham'
+    pass
+
+def test_jwt_encode():
+    pass
+
+def test_jwt_decode():
+    pass
+
