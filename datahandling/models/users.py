6241fcb8637d82c1c414b69762a820cc8b9aa3f4
Samuel Kerr <kuzmatech@gmail.com>
2018-11-18 09:38:15 +0000

    renamed id to prevent override

--- datahandling/models/users.py
+++ datahandling/models/users.py
@@ -2,11 +2,12 @@ from ..db import database
 
 class User:
     TABLE_ID = "USER"
-    def __init__(self, id: str, username: str, password: str, salt: str):
+    def __init__(self, id_: str, username: str, password: str, salt: str):
+        self.id_ = id_
         self.username = username
         self.password = password
         self.salt = salt
     @classmethod
     def from_database(cls, uid):
         db_results = database.getone(User).by(id=uid)
-        return cls(id=db_results['id'], username=db_results['username'], password=db_results['password'], salt=db_results['salt'])
+        return cls(id_=db_results['id'], username=db_results['username'], password=db_results['password'], salt=db_results['salt'])
