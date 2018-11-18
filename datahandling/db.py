16fefa2d30c99bbc120de16ca766f34c6c1805d2
Samuel Kerr <kuzmatech@gmail.com>
2018-11-18 12:08:24 +0000

    Test fixes
    Update implementation and made some updates in order to pass tests

--- datahandling/db.py
+++ datahandling/db.py
@@ -2,22 +2,62 @@ import sqlite3
 import re
 
 class sqllitedb:
+    @staticmethod
+    def dict_factory(cursor, row):
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
     def __init__(self, connectionstring: str):
-        self.connectionstring = 'data/{0}.db'.format(connectionstring)
+        if connectionstring[0] == ':':
+            self.connectionstring = connectionstring
+        else:
+            self.connectionstring = 'data/{0}.db'.format(connectionstring)
     def __enter__(self):
-        self.conn = sqlite3.connect(self.connectionstring, row_factory=sqlite3.Row)
+        self.conn = sqlite3.connect(self.connectionstring)
+        self.conn.row_factory = sqllitedb.dict_factory
         return self.conn.cursor()
     def __exit__(self, type, value, traceback):
         self.conn.commit()
         self.conn.close()
+    def execute(self, sql, parameters = {}):
+        conn = sqlite3.connect(self.connectionstring)
+        return conn.execute(sql, parameters)
+    def commit(self):
+        conn = sqlite3.connect(self.connectionstring)
+        return conn.commit()
 
 
 class db:
     def __init__(self, name: str, dbtype: str = "SQLite"):
         self.database = sqllitedb(name)
         self.dbtype = dbtype
-    def getone(self, type):
-        return dbquery(database, type.TABLE_ID)
+    def getone(self, type_):
+        return dbquery(self, type_.TABLE_ID)
+    def update(self, type, data: dict):
+        if not data['id']:
+            raise SyntaxError
+        with self.database as conn:
+            if(self.dbtype == "SQLite"):
+                for key, value in data.items():
+                    querystring = "UPDATE {0} SET {1} = ? WHERE id=?".format(type.TABLE_ID, key)
+                    dataidobj = {"id": data['id']}
+                    conn.execute(querystring, (value, dbquery.querystringgenerator(dataidobj))) 
+    def add(self, type, data: dict):
+        with self.database as conn:
+            if(self.dbtype == "SQLite"):
+                keyarray = []
+                valuearray = []
+                for key in data.keys():
+                    keyarray.append(key)
+                    valuearray.append(':{0}'.format(key))
+                keyarraystring = ', '.join(keyarray)
+                valuearraystring = ', '.join(valuearray)
+                querystring = "INSERT INTO {0}({1}) VALUES({2})".format(type.TABLE_ID, keyarraystring, valuearraystring)
+                conn.execute(querystring, data)
+                return conn.lastrowid
+
+
 
 class dbquery:
     def __init__(self, db:db, tableid: str):
@@ -25,8 +65,12 @@ class dbquery:
         self.tableid = tableid
     @staticmethod
     def querystringgenerator(queryargs: dict) -> str:
-        def sanitisedarg(key, value):
-            return "{0} IS {1}".format(key, (re.sub(r"[^A-Za-z\d.-]", "", value)))
+        def sanitisedarg(key, value, operation: str = "string"):
+            if operation == "string":
+                return "{0} IS {1}".format(key, (re.sub(r"[^A-Za-z\d.-]", "", value)))
+            if operation == "value":
+                return re.sub(r"[^A-Za-z\d.-]", "", value)
+
         if(len(queryargs) == 1):
             singlequery = ''
             for key, value in queryargs.items():
@@ -41,9 +85,9 @@ class dbquery:
 
     def by(self, **kargs):
         with self.db.database as conn:
-            if(conn.dbtype == "SQLite"):
+            if(self.db.dbtype == "SQLite"):
                 querystring = dbquery.querystringgenerator(kargs)
-                formattedquerystring = "SELECT 1 FROM {0} WHERE {1}".format(self.tableid, querystring)
+                formattedquerystring = "SELECT * FROM {0} WHERE {1}".format(self.tableid, querystring)
                 conn.execute(formattedquerystring)
                 return conn.fetchone()
 
--- datahandling/models/users.py
+++ datahandling/models/users.py
@@ -2,7 +2,7 @@ from ..db import database
 
 class User:
     TABLE_ID = "USER"
-    def __init__(self, id_: str, username: str, password: str, salt: str):
+    def __init__(self, username: str, password: str, salt: str, id_: str = None):
         self.id_ = id_
         self.username = username
         self.password = password
@@ -11,3 +11,9 @@ class User:
     def from_database(cls, uid):
         db_results = database.getone(User).by(id=uid)
         return cls(id_=db_results['id'], username=db_results['username'], password=db_results['password'], salt=db_results['salt'])
+    def store(self):
+        try:
+            database.getone(User).by(id=self.id_)
+            return database.update(User, self)
+        except ReferenceError:
+            return database.add(User, self)
